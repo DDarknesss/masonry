2fb0f8fc9d1be19841ad5d2e27532dfb727b1dd4
Davit <davit@Davits-MacBook-Air.local>
2019-06-01 14:19:06 +0400

    eslint

--- mosaic.js
+++ mosaic.js
@@ -9,6 +9,7 @@
     }, options);
  
     function mainObject(mainContainer, settings){
+
         this.mainContainer = mainContainer;
         this.liImg = this.mainContainer.find('li');
         this.image = this.mainContainer.find('img');
@@ -21,85 +22,9 @@
                 that.calculation(that.mainContainer.width() < that.settings.imagesWidth*2);
             });
 
-            this.image.each(function() {
-                $(this).css('margin-bottom' ,that.settings.imegesGap);
-            });
-
             this.hoverEffect();
         };
-
-        mainObject.prototype.calculation = function (single){
-            var that = this;
-            that.array_of_heights = [];
-
-            that.itemWidth = that.settings.imagesWidth + that.settings.imegesGap;
-            that.columns = Math.floor(that.mainContainer.width()/that.itemWidth);
-            that.mainContainer.css('width',that.mainContainer.parent().width());
-            that.current_image_high = 0;
-            that.current_column = 0;
-            that.lowest_column = 0;
-            that.columnHeight = 0;
-            that.chunk = [];
- 
-
-        that.liImg.each(function(index){              
- 
-            that.top = 0;
-
-            if(single) {
-                that.current_column = 0;
-                that.left = that.settings.imegesGap;
-            } else {
-                that.current_column = that.lowest_column;
-                that.left = that.itemWidth*that.current_column;
-            };
-
-            for(var rm = 0; rm < that.columns; rm++) {
-                $(this).removeClass('column' + rm);
-            };
-
-            $(this).addClass('column' + that.current_column);
-
-            $(this).css({
-                'width': that.settings.imagesWidth,
-                'position': 'absolute',
-                'left': that.left,
-            });      
-
-            that.current_image_high = Math.ceil($(this).height() + that.settings.imegesGap);
-
-            console.log(that.top);
-
-            if(that.array_of_heights.length >= that.columns){
-                that.array_of_heights[that.lowest_column] += that.current_image_high;
-            } else {
-                that.array_of_heights.push(that.current_image_high);
-            };
-            
-            console.log(that.array_of_heights)
-            
-            that.lowest_column = that.array_of_heights.indexOf(Math.min.apply(Math,that.array_of_heights));         
-            that.top = that.array_of_heights[that.lowest_column];
-
-            console.log(that.top)
-
-            $(this).css('top',that.top);
-    });
-
-};
-
-
-        mainObject.prototype.chunkArray = function (myArray, chunk_size){
-            this.results = [];
-            
-            while (myArray.length) {
-                this.results.push(myArray.splice(0, chunk_size));
-            };
-
-            return this.results;
-        };
-
-
+        
         mainObject.prototype.hoverEffect = function (){
             if(this.settings.hoverTitle){
                 this.image
@@ -119,6 +44,66 @@
             } else {
                 this.fontSize = 11;
             };
+        };
+
+        mainObject.prototype.calculation = function (single){
+            var that = this;
+
+            that.itemWidth = that.settings.imagesWidth + that.settings.imegesGap;
+            that.columns = Math.floor(that.mainContainer.width()/that.itemWidth);
+            that.mainContainer.css('width',that.mainContainer.parent().width());
+            that.array_of_heights = Array(that.columns).fill(0);
+            that.current_image_high = 0;
+            that.current_column = 0;
+            that.lowest_column = 0;
+            that.columnHeight = 0;
+            that.top = 0;
+
+        that.liImg.each(function(){
+
+            
+
+
+            if(single) {
+                that.current_column = 0;
+                that.left = that.settings.imegesGap;
+            } else {
+                that.current_column = that.lowest_column;
+                that.left = that.itemWidth*that.current_column;
+            };
+            
+            for(var rm = 0; rm < that.columns; rm++) {
+                $(this).removeClass('column' + rm);
+            };
+
+            $(this)
+                .addClass('column' + that.current_column)
+                .css({
+                'width': that.settings.imagesWidth,
+                'position': 'absolute',
+                'left': that.left,
+                });      
+
+                
+            that.current_image_high = Math.ceil($(this).height() + that.settings.imegesGap);
+            
+            
+            if(that.array_of_heights.length >= that.columns){
+                that.array_of_heights[that.lowest_column] += that.current_image_high;
+            };
+            
+            that.lowest_column = that.array_of_heights.indexOf(Math.min.apply(Math,that.array_of_heights));    
+            that.top = that.array_of_heights[that.lowest_column];
+            
+            console.log(this,that.array_of_heights, that.lowest_column);
+
+
+            that.highest_column = Math.max.apply(Math,that.array_of_heights)
+
+            $(this).css('top',that.top);
+            that.mainContainer.css('height',that.highest_column+100 );
+        });
+
         };
 
     };
@@ -141,15 +126,18 @@ $(document).ready(function(){
         imagesWidth: 200,
         imegesGap: 10,
     }); 
+    
+    $('ul.mosaic2').pluginJQ({
+        hoverTitle: true, 
+        imagesWidth: 250,
+        imegesGap: 20,
+    }); 
+
+
 });
 
 
-    // $('ul.mosaic2').pluginJQ({
-    //     hoverTitle: true, 
-    //     imagesWidth: 250,
-    //     imegesGap: 20,
-    // }); 
-
+   
     // $('ul.mosaic3').pluginJQ({
     //     hoverTitle: true, 
     //     imagesWidth: 300,
